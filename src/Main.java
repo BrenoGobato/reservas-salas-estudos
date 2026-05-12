45a5e73e2cb2ebd85304d423db4e6b29ef7d7890
BrenoGobato <bmgobato@gmail.com>
2026-05-12 17:53:03 -0300

    feat: alteracao da main para teste do repository

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 import factory.SalaFactory;
 import factory.TipoSala;
 import model.Sala;
+import repository.ReservaRepository;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        ReservaRepository repository = ReservaRepository.getInstance();
+
         Sala sala1 = SalaFactory.criarSala(TipoSala.INDIVIDUAL, "Sala 101");
         Sala sala2 = SalaFactory.criarSala(TipoSala.GRUPO, "Sala 202");
-        Sala sala3 = SalaFactory.criarSala(TipoSala.LABORATORIO, "Lab 01");
 
-        System.out.println(sala1.getNome());
-        System.out.println(sala2.getNome());
-        System.out.println(sala3.getNome());
+        repository.adicionarSala(sala1);
+        repository.adicionarSala(sala2);
+
+        for (Sala sala : repository.listarSalas()) {
+            System.out.println(sala.getNome());
+        }
     }
 }
